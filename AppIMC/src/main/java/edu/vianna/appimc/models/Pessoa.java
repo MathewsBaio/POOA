85f6bd0c4b5c4c6a5d095f092de8a27e3ef95aaa
Mathews <baiomathews@gmail.com>
2025-02-24 16:55:20 -0300

    testando

---
+++ AppIMC/src/main/java/edu/vianna/appimc/models/Pessoa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vianna.appimc.models;
+
+/**
+ *
+ * @author Mathews
+ */
+public class Pessoa {
+    private String nome;
+    private double peso;
+    private double altura;
+    
+    public Pessoa(){}
+    public Pessoa(String nome, double peso, double altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+            
+}
