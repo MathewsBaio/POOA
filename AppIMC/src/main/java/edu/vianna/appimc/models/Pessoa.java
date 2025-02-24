643db184724b2f1943a3057678130af552167970
Mathews <baiomathews@gmail.com>
2025-02-24 16:59:18 -0300

    getter e setters, calculo de IMC

--- AppIMC/src/main/java/edu/vianna/appimc/models/Pessoa.java
+++ AppIMC/src/main/java/edu/vianna/appimc/models/Pessoa.java
@@ -19,5 +19,32 @@ public class Pessoa {
         this.peso = peso;
         this.altura = altura;
     }
-            
+
+    public double IMC() {
+        return peso * (altura * altura);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 }
